3d742569e98ee45c79cc67dc9ed3f8b1c8b87722
xxxasdfghjk <xxxasdfghjk999@gmail.com>
2024-02-25 23:43:12 +0900

    fix viewport

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
+        <App />
+    </React.StrictMode>
+);
